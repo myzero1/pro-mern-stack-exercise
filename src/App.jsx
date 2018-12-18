a8bac91a255c8074963868e6e327291596d1325e
myzero1 <myzero1@sina.com>
2018-12-19 05:10:03 +0800

    holdon

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,50 @@ class IssueFilter extends React.Component{
 
 class IssueTable extends React.Component{
 	render(){
+		const borderedStyle = {border:"1px solid silver",padding:6};
+
 		return (
-			<div>This is a placehoder for issue table.</div>
+			<table style={{borderCollapse:"collapse"}}>
+				<thead>
+					<tr>
+						<th style={borderedStyle}>ID</th>
+						<th style={borderedStyle}>Title</th>
+					</tr>
+				</thead>
+				<tbody>
+					<IssueRow issue_id={1} issue_title="this is first issue row." />
+					<IssueRow issue_id={2} issue_title="this is 2 issue row." />
+					<IssueRow issue_id={3} />
+				</tbody>
+			</table>
 		)
+	}
+}
+
+class IssueRow extends React.Component{
+	render(){
+		const borderedStyle = {border:"1px solid silver", padding:4};
+
+		return(
+			<tr>
+				<td style={borderedStyle}>{this.props.issue_id}</td>
+				<td style={borderedStyle}>{this.props.issue_title}</td>
+			</tr>
+		)
+	}
+
+	static get propTypes(){
+		return {
+			issue_id:React.PropTypes.number.isRequired,
+			issue_title:React.PropTypes.string
+		};
+	}
+
+	static get defaultProps(){
+		return {
+			issue_id:"-----no id------",
+			issue_title:"-----no title------"
+		};
 	}
 }
 
